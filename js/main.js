1227bc289249ed49d74c10aee1e2ba7c8acec6c8
Oliver <omh.1999hs@gmail.com>
2018-12-16 22:54:21 +0000

    Complete Design Rework

--- js/main.js
+++ js/main.js
@@ -1,52 +1,19 @@
-// Header Splash Screen
-/* to track screen position */
 let pos;
 
-/*
-  On page load: set header height to not interfer when javascript is disabled
-*/
-window.onload = () => { /* run after contents loaded */
-//  document.getElementById('hd').style.height = "100vh";
-  if (checkAnchor() == true) {
-    document.getElementById('hd').classList.add("splash");
-  }
-  document.getElementById('hd').style.position = "fixed";
-  document.getElementById('menu').classList.add("hidden");
-
-//  if there is an article, append to page title
-  let article = document.getElementById('article');
-  if (article != null) {
-    document.title = `Byte Planet | ${article.dataset.title}`
-  }
+window.onload = () => {
+  document.getElementById('nav').classList.remove("noJS");
 };
 
-function checkAnchor() {
-  let url = document.URL;
+window.addEventListener('scroll', () =>{
 
-  let bookmark = url.split('#');
-
-  if (bookmark[1] == null) {
-    return true;
-  }
-}
-
-/*
-  run arrow function on page scroll
-*/
-window.addEventListener('scroll', () => {
-  // update pos
   pos = (document.body.getBoundingClientRect()).top;
 
-  if (pos < -10) {
-
-    document.getElementById('hd').classList.remove("splash");
-    document.getElementById('menu').classList.remove("hidden");
-
+  if (pos == 0) {
+    document.getElementById('nav').classList.remove("responsive");
+    document.getElementById('splash').classList.remove("responsive");
   } else {
-
-    document.getElementById('hd').classList.add("splash");
-    document.getElementById('menu').classList.add("hidden");
-
-  }
+    document.getElementById('nav').classList.add("responsive");
+    document.getElementById('splash').classList.add("responsive");
+  };
 
 });
