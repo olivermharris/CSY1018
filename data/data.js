d9f18541202fd9401fd6c78643b6f2ce8aa9a20a
Oliver <omh.1999hs@gmail.com>
2018-12-30 14:25:09 +0000

    authors size adjustment data and js setup for changing content comment remap

---
+++ data/data.js
@@ -0,0 +1,29 @@
+const data = {
+  authors: [
+    {
+      name: "Marcus Hemlin",
+      role: "Lead Editor",
+      about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      picture: "john.jpeg",
+      articles: [
+        {
+          title: "The Importance of SSL Certificates",
+          link: ""
+        },
+        {
+          title: "",
+          link: ""
+        }
+      ],
+      socialmedia: {
+        github: "https://github.com/#",
+        twitter: "https://twitter.com/#",
+        blog: "https://example.com/#",
+        linkedin: "https://linkedin.com/#"
+      }
+    },
+    {
+
+    }
+  ]
+}
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ let pos;
 
 window.onload = () => {
   document.getElementById('nav').classList.remove("noJS");
+
 };
 
 window.addEventListener('scroll', () =>{
@@ -17,3 +18,43 @@ window.addEventListener('scroll', () =>{
   };
 
 });
+
+function getVars(variable) {
+
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+
+  for (var i=0;i<vars.length;i++) {
+     var pair = vars[i].split("=");
+     if(pair[0] == variable){return pair[1];}
+   }
+
+  return(false);
+
+};
+
+function clearAuthor() {
+  document.getElementById('author-details').classList.add("hidden");
+  document.getElementById('author-details').innerHTML = "";
+}
+
+function loadAuthor(authorID) {
+  document.getElementById('author-details').classList.remove("hidden");
+
+  let author = data.authors[authorID];
+
+  document.getElementById('author-details').innerHTML = `
+  <section class="marker" id="author"></section>
+  <section class="heading sub">
+
+    <h1><a href="#authors" onclick="clearAuthor()">${author.name}</a></h1>
+
+  </section>
+
+  <section>
+
+    <p>${author.about}</p>
+
+  </section>
+  `
+};
