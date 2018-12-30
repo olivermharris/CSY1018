43d525215ede6ded4ac26f5861b6d37a421a4c54
Oliver <omh.1999hs@gmail.com>
2018-12-30 21:16:24 +0000

    images and authors content /layout

--- data/data.js
+++ data/data.js
@@ -3,8 +3,8 @@ const data = {
     {
       name: "Marcus Hemlin",
       role: "Lead Editor",
-      about: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      picture: "john.jpeg",
+      about: "",
+      picture: "mhemlin.jpg",
       articles: [
         {
           title: "The Importance of SSL Certificates",
@@ -23,7 +23,48 @@ const data = {
       }
     },
     {
-
+      name: "Shayne Hurst",
+      role: "Content Manager",
+      about: "",
+      picture: "shurst.jpg",
+      articles: [
+        {
+          title: "The Importance of SSL Certificates",
+          link: ""
+        },
+        {
+          title: "",
+          link: ""
+        }
+      ],
+      socialmedia: {
+        github: "https://github.com/#",
+        twitter: "https://twitter.com/#",
+        blog: "https://example.com/#",
+        linkedin: "https://linkedin.com/#"
+      }
+    },
+    {
+      name: "Katlyn Kilton",
+      role: "Lead Researcher",
+      about: "",
+      picture: "kkilton.jpg",
+      articles: [
+        {
+          title: "The Importance of SSL Certificates",
+          link: ""
+        },
+        {
+          title: "",
+          link: ""
+        }
+      ],
+      socialmedia: {
+        github: "https://github.com/#",
+        twitter: "https://twitter.com/#",
+        blog: "https://example.com/#",
+        linkedin: "https://linkedin.com/#"
+      }
     }
   ]
 }
