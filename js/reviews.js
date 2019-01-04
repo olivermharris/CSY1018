d30e10a439e69e2723bffbad87a5223f78883273
Oliver <omh.1999hs@gmail.com>
2019-01-04 00:54:16 +0000

    review layout fix

--- js/reviews.js
+++ js/reviews.js
@@ -3,13 +3,19 @@ let reviewdata = [
     title: "Blogging Technology",
     author: 0,
     //
-    content: "<section><p> For developers, blogging has become a prominent tool to update your clients and log your own work, but what technology should a developer use?</p><p> We at <b>Byte Planet Publishing</b> have reviewed two useful blog assets (<b>Wordpress</b> and <b>Jackyl</b>). To determine the ups and the downs. But most importantly, what looks best when used by a developer. Firstly to determine the right back-end architecture for a project, the developer must choose a platform designed for their solution.</p></section><section class=\”flex11\”><section></section><section></section></section>"
+    content: "<section><p>For developers, blogging has become a prominent tool to update your clients and log your own work, but what technology should a developer use?</p><p>We at <b>Byte Planet Publishing</b> have reviewed two useful blog assets (<b>Wordpress</b> and <b>Jackyl</b>). To determine the ups and the downs. But most importantly, what looks best when used by a developer. Firstly to determine the right back-end architecture for a project, the developer must choose a platform designed for their solution.</p></section><section class=\"flex11\"><section><p>test</p></section><section><h3> Jackyl</h3><p><b>Jackyl</b> is a static site generator, this is intended for more advanced developers to create a blog, log, etc.. almost from scratch.</p><h3>Benefits</h3><ul><li>Fully customisable</li><li>Hackable</li><li>Programable</li></ul><h3>Disadvantages</h3><ul><li>Difficult to start</li><li>From Scratch</li></ul></section></section><section><p>To conclude, choosing the right logging or blogging architecture is up to the developer, but creating a less uniform design will make them stand out from the rest.</p></section>"
   },
   {
-    title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    title: "test",
     author: 2,
     //
-    content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    content: "<section><p>For developers, blogging has become a prominent tool to update your clients and log your own work, but what technology should a developer use?</p><p>We at <b>Byte Planet Publishing</b> have reviewed two useful blog assets (<b>Wordpress</b> and <b>Jackyl</b>). To determine the ups and the downs. But most importantly, what looks best when used by a developer. Firstly to determine the right back-end architecture for a project, the developer must choose a platform designed for their solution.</p></section><section class=\"flex11\"><section><p>test</p></section><section><h3> Jackyl</h3><p><b>Jackyl</b> is a static site generator, this is intended for more advanced developers to create a blog, log, etc.. almost from scratch.</p><h3>Benefits</h3><ul><li>Fully customisable</li><li>Hackable</li><li>Programable</li></ul><h3>Disadvantages</h3><ul><li>Difficult to start</li><li>From Scratch</li></ul></section></section><section><p>To conclude, choosing the right logging or blogging architecture is up to the developer, but creating a less uniform design will make them stand out from the rest.</p></section>"
+  },
+  {
+    title: "",
+    author: 2,
+    //
+    content: ""
   }
 ]
 
